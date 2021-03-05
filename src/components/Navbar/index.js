0f4dc0af1f0242e10994f9ca5b0d0805da65db85
Robby Ray <rgr5035@gmail.com>
2021-03-04 17:36:30 -0700

    added links

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,21 +1,40 @@
 import React from "react";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
+import { Link } from "react-router-dom";
 import PDF from "./RR.CV.2021.INT.pdf";
 import "./style.css";
 
 export default function PortfolioNavBar() {
   return (
     <Navbar bg="dark" expand="lg" variant="dark" className="shadow-lg navbar">
-      <Navbar.Brand href="/about">Robby Ray</Navbar.Brand>
+      <Navbar.Brand>
+        <Link style={{ textDecoration: "none", color: "inherit" }} to="/about">
+          Robby Ray
+        </Link>
+      </Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="mr-auto">
-          <Nav.Link href="/portfolio">Portfolio</Nav.Link>
+          <Nav.Link>
+            <Link
+              style={{ textDecoration: "none", color: "inherit" }}
+              to="/portfolio"
+            >
+              Portfolio
+            </Link>
+          </Nav.Link>
           <Nav.Link href={PDF} target="_blank">
             Resume
           </Nav.Link>
-          <Nav.Link href="/contact">Contact</Nav.Link>
+          <Nav.Link>
+            <Link
+              style={{ textDecoration: "none", color: "inherit" }}
+              to="/contact"
+            >
+              Contact
+            </Link>
+          </Nav.Link>
         </Nav>
       </Navbar.Collapse>
     </Navbar>
