86b177b37c9ba6b4dc43eeec4f38fcd9647d36ad
Robby Ray <rgr5035@gmail.com>
2021-03-04 17:14:09 -0700

    troubleshoot deploy

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import Wrapper from "./components/Wrapper";
+// import Wrapper from "./components/Wrapper";
 import About from "./pages/about";
 import Portfolio from "./pages/portfolio";
 import Contact from "./pages/contact";
@@ -13,12 +13,12 @@ function App() {
     <div className="App">
       <Router>
         <Navbar />
-        <Wrapper>
-          <Route exact path="/" component={About} />
-          <Route exact path="/about" component={About} />
-          <Route exact path="/portfolio" component={Portfolio} />
-          <Route exact path="/contact" component={Contact} />
-        </Wrapper>
+        {/* <Wrapper> */}
+        <Route exact path="/" component={About} />
+        <Route exact path="/about" component={About} />
+        <Route exact path="/portfolio" component={Portfolio} />
+        <Route exact path="/contact" component={Contact} />
+        {/* </Wrapper> */}
         <Footer />
       </Router>
     </div>
